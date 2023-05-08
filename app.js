7f11f60bbdf62e6d04db08ac6186be3ce21424e4
Ganesh Pilli <neghani@gmail.com>
2023-05-08 17:13:35 +0530

    good version

--- app.js
+++ app.js
@@ -1,36 +1,56 @@
 // CREATE AN EDITOR
 // Initial data
-let JS_CODE = `    console.log("hello")    `;
+let code = `    console.log("hello")    `;
+let lang = "javascript";
 var h_div = document.getElementById("editor");
 var preview = document.getElementById("preview");
-var editor = monaco.editor.create(h_div, {
-  value: JS_CODE,
-  language: "javascript",
-});
-
-var run = () => {
-  preview.innerHTML = "";
-  try {
-    var value = eval(editor.getValue());
-  } catch (err) {
-    preview.innerHTML = "<pre class='error'>" + err + "</pre>";
-  }
-};
-
+var editor;
+let file_path;
+const editorPreview =
+  document.getElementById("editorPreview").contentWindow.document;
 (function () {
   var oldLog = console.log;
   console.log = function (message) {
-    // DO MESSAGE HERE
-
-    preview.innerHTML += message + "</br>";
+    preview.innerHTML += "> " + message + "</br>";
     oldLog.apply(console, arguments);
   };
+
+  document.write = function (message) {
+    preview.innerHTML += message;
+  };
 })();
-const urlSearchParams = new URLSearchParams(window.location.search);
-const params = Object.fromEntries(urlSearchParams.entries());
-if (params.file) {
-  readTextFile("./files/" + params.file);
+
+function run() {
+  let value = editor.getValue();
+  if (lang == "html") {
+    HTMLRun();
+  } else {
+    javascriptRun(value);
+  }
 }
+
+function HTMLRun() {
+  editorPreview.body.innerHTML = editor.getValue();
+}
+function javascriptRun(value) {
+  preview.innerHTML = "";
+  try {
+    eval(value);
+  } catch (err) {
+    preview.innerHTML = "<pre class='error'> > " + err + "</pre>";
+  }
+}
+function setConfigurations() {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+  if (params.file) {
+    file_path = "./files/" + params.file;
+  }
+  if (params.language) {
+    lang = params.language;
+  }
+}
+
 function readTextFile(file) {
   var rawFile = new XMLHttpRequest();
   rawFile.open("GET", file, false);
@@ -38,10 +58,21 @@ function readTextFile(file) {
     if (rawFile.readyState === 4) {
       if (rawFile.status === 200 || rawFile.status == 0) {
         var allText = rawFile.responseText;
+        console.log(allText);
         editor.setValue(allText);
         preview.innerHTML = "";
       }
     }
   };
   rawFile.send(null);
+}
+
+function init() {
+  editor = monaco.editor.create(h_div, {
+    value: code,
+    language: lang,
+    fontSize: "16px",
+    theme: "vs-dark",
+  });
+  readTextFile(file_path);
 }
